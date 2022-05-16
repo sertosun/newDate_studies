ace41ecf8264a1fcd0e0123bc27ba36665774b06
Sadeddin Ertosun <82061290+sertosun@users.noreply.github.com>
2022-05-17 00:37:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const d = new Date();
-let hours = d.getHours();
-let text = d.toLocaleTimeString();
+function new_clock() {
+  const d = new Date();
+  let hours = d.getHours();
+  let text = d.toLocaleTimeString();
 
-let demo = document.getElementById("demo");
-demo.innerHTML = text;
+  let actTime = document.getElementById("actTime");
+  actTime.innerHTML = text;
 
-let box = document.getElementById("box");
+  let box = document.getElementById("box");
 
-if (hours >= 0 && hours <= 6) {
-  box.style.backgroundColor = "blue";
-} else if (hours >= 18 && hours <= 23) {
-  box.style.backgroundColor = "black";
+  let subTit = document.getElementById("subTit");
+
+  if (hours >= 0 && hours <= 6) {
+    box.style.backgroundColor = "blue";
+  } else if (hours >= 6 && hours <= 12) {
+    box.style.backgroundColor = "yellow";
+  } else if (hours >= 12 && hours <= 18) {
+    box.style.backgroundColor = "red";
+  } else {
+    box.style.backgroundColor = "black";
+  }
+
+  setTimeout(new_clock, 1000);
 }
+
+new_clock();
