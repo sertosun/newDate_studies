5f85e960df55d45b8b145c4617550e0008801a9e
Sadeddin Ertosun <82061290+sertosun@users.noreply.github.com>
2022-05-17 08:54:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ function new_clock() {
 
   let box = document.getElementById("box");
 
-  let subTit = document.getElementById("subTit");
-
   if (hours >= 0 && hours <= 6) {
+    let subTit = document.getElementById("subTit");
     box.style.backgroundColor = "blue";
+    subTit.style.color = "yellow";
   } else if (hours >= 6 && hours <= 12) {
     box.style.backgroundColor = "yellow";
+    subTit.style.color = "red";
   } else if (hours >= 12 && hours <= 18) {
     box.style.backgroundColor = "red";
+    subTit.style.color = "yellow";
   } else {
     box.style.backgroundColor = "black";
+    subTit.style.color = "white";
   }
 
   setTimeout(new_clock, 1000);
